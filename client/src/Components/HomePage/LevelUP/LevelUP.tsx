2e19a6ed99106f85ff0bb7bafd0af4ac05da9382
Khaled <khaledghonim89@gmail.com>
2025-03-28 23:31:56 +0300

    Enhance LevelUP component with additional images and improved animations for better visual engagement

--- client/src/Components/HomePage/LevelUP/LevelUP.tsx
+++ client/src/Components/HomePage/LevelUP/LevelUP.tsx
@@ -1,5 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion";
 import LevelUPImg from "../../../assets/LevelUP.avif";
+import LevelUP2 from "../../../assets/LevelUP2.avif";
+import LevelUP3 from "../../../assets/LevelUP3.avif";
 
 function LevelUP() {
   const parentVariants = {
@@ -26,30 +28,61 @@ function LevelUP() {
       <section className="w-full pt-20 lg:pt-28 h-dvh SectionS px-8 lg:px-16 !overflow-hidden relative flex justify-center items-center">
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
+          whileInView={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0 }}
+          whileHover={{ scale: 1.1 }}
+
           transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
-          className="w-[30rem] h-[30rem] z-40"
+          className="w-56 lg:w-96 cursor-pointer z-40 lg:h-[30rem] z-40"
         >
           <img
             src={LevelUPImg}
-            className="w-full h-full rounded-4xl z-40 object-cover"
+            className="w-full h-full rounded-4xl  object-cover"
             alt=""
           />
-        </motion.div>{" "}
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0 }}
+          whileHover={{ scale: 1.1 }}
+
+          transition={{ duration: 0.5, delay: 1, ease: "easeInOut" }}
+          className="w-52  lg:h-80 z-40 cursor-pointer absolute left-20 top-2/12 rotate-45"
+        >
+          <img
+            src={LevelUP2}
+            className="w-full h-full rounded-4xl  object-cover"
+            alt=""
+          />
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, delay: 1.5, ease: "easeInOut" }}
+          className="w-52  lg:h-80 z-40 cursor-pointer absolute right-20 bottom-1/12 -rotate-45"
+        >
+          <img
+            src={LevelUP3}
+            className="w-full h-full rounded-4xl  object-cover"
+            alt=""
+          />
+        </motion.div>
         <motion.div
           variants={parentVariants}
           initial="hidden"
           whileInView="visible"
           exit="exit"
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-12  text-center max-w-7xl  w-full font-bold stick-bold"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:-rotate-12  text-center max-w-7xl  w-full font-bold stick-bold"
         >
-          <h1 className="text-[100px] lg:text-[150px] leading-[150px] text-zinc-400 text-center">
-          Welcome to VEXO
+          <h1 className="text-[100px] lg:text-[150px]  z-20 leading-[220px] lg:leading-[240px] text-zinc-400 text-center">
+            Welcome to VEXO
           </h1>
         </motion.div>
-
-        
       </section>
     </AnimatePresence>
   );
