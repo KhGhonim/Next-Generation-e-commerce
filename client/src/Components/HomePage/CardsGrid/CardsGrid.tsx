c92f66c5fb4a727225d5b6e064ecc274e2e3b4c4
Khaled <khaledghonim89@gmail.com>
2025-03-29 00:35:51 +0300

    Add "See All Brands" button with icon to CardsGrid component for enhanced navigation

--- client/src/Components/HomePage/CardsGrid/CardsGrid.tsx
+++ client/src/Components/HomePage/CardsGrid/CardsGrid.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion";
 import Cards from "../Cards/Cards";
 import { PhotosAndTitle } from "../../../Context/Context";
+import { MdOutlineArrowOutward } from "react-icons/md";
 
 function CardsGrid() {
   return (
@@ -21,6 +22,16 @@ function CardsGrid() {
         <div className="w-full h-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 my-5 place-items-center  lg:px-24">
           <Cards cards={PhotosAndTitle} />
         </div>
+
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          className="w-full h-32 flex gap-1.5 justify-center items-center"
+        >
+          <button className="bg-black cursor-pointer text-white px-10 py-3 rounded-3xl">
+            See All Brands
+          </button>
+          <MdOutlineArrowOutward className="bg-black cursor-pointer !text-3xl text-white p-1.5  rounded-3xl" />
+        </motion.div>
       </section>
     </AnimatePresence>
   );
