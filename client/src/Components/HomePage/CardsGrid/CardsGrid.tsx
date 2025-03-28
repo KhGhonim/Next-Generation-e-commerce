b1f99c1dabf4f5c6dcdb9072bc432a9310b43e2b
Khaled <khaledghonim89@gmail.com>
2025-03-29 00:47:28 +0300

    Enhance CardsGrid component with animation effects for improved visual engagement and responsiveness

--- client/src/Components/HomePage/CardsGrid/CardsGrid.tsx
+++ client/src/Components/HomePage/CardsGrid/CardsGrid.tsx
@@ -4,24 +4,61 @@ import { PhotosAndTitle } from "../../../Context/Context";
 import { MdOutlineArrowOutward } from "react-icons/md";
 
 function CardsGrid() {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3, 
+        delayChildren: 0.2, 
+      },
+    },
+  };
+
   return (
     <AnimatePresence mode="wait">
       <section className="w-full pt-20 lg:pt-28  h-full SectionS   !overflow-hidden relative ">
-        <div className="w-full h-24 lg:h-32 text-center flex justify-between items-center">
-          <motion.h6 className="stick-regular w-1/5 text-xs">
+        <motion.div
+          variants={containerVariants}
+          initial="hidden"
+          whileInView="visible"
+          className="w-full h-24 lg:h-32 text-center flex justify-between items-center"
+        >
+          <motion.h6
+            initial={{ opacity: 0, x: 100, y: 100 }}
+            whileInView={{ opacity: 1, x: 0, y: 0 }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+            className="stick-regular w-1/5 text-xs"
+          >
             New Arrivals
           </motion.h6>
-          <motion.h1 className="stick-bold text-lg  w-3/5 lg:text-6xl ">
+          <motion.h1
+            initial={{ opacity: 0, x: 100, y: 100 }}
+            whileInView={{ opacity: 1, x: 0, y: 0 }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+            className="stick-bold text-lg  w-3/5 lg:text-6xl "
+          >
             Fresh Fits For Your Next Workout!
           </motion.h1>
-          <motion.h1 className="stick-regular  w-1/5 text-xs ">
+          <motion.h1
+            initial={{ opacity: 0, x: 100, y: 100 }}
+            whileInView={{ opacity: 1, x: 0, y: 0 }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+            className="stick-regular  w-1/5 text-xs "
+          >
             All Brands
           </motion.h1>
-        </div>
+        </motion.div>
 
-        <div className="w-full h-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 my-5 place-items-center  lg:px-24">
+        <motion.div
+          initial={{ opacity: 0, x: 100, y: 100 }}
+          whileInView={{ opacity: 1, x: 0, y: 0 }}
+          exit={{ opacity: 0, x: 100, y: 100 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-full h-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 my-5 place-items-center  lg:px-24"
+        >
           <Cards cards={PhotosAndTitle} />
-        </div>
+        </motion.div>
 
         <motion.div
           whileHover={{ scale: 1.1 }}
